e60805f370b4671d776fa869b6bf60242fee35ec
Jay Viray <64195784+virayjj@users.noreply.github.com>
2021-04-12 19:59:24 +0100

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,14 @@
+import pygame
+
+# colours
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+
+# screen
+size = (1280, 720)
+screen = pygame.display.set_mode((size), pygame.FULLSCREEN)
+pygame.display.set_caption("Test1")
