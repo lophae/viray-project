59674a0342879de5b11413bbba527b4aedeab6b6
Jay Viray <64195784+virayjj@users.noreply.github.com>
2021-09-13 20:16:44 +0100

    Update main.py

--- main.py
+++ main.py
@@ -381,6 +381,7 @@ def enemyShoot():
                 enemybullet_group.add(eb)
                 all_sprites_list.add(eb)
 
+
 def bossAttack1():
     for foo in boss_group1:
         foo.attack(player.rect.x, player.rect.y)
