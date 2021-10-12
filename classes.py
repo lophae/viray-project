3d4b3579f0bd698edd4a80664cae68c09cae51ee
Jay Viray <64195784+virayjj@users.noreply.github.com>
2021-10-12 09:43:57 +0100

    test

--- classes.py
+++ classes.py
@@ -148,7 +148,7 @@ class Boss1(pygame.sprite.Sprite):
         angle = math.atan2(self.target_y-self.rect.y, self.target_x-self.rect.x)
         self.dx = math.cos(angle) * 4
         self.dy = math.sin(angle) * 4
-        self.health = 25
+        self.health = 1
 
     def attack(self):
         self.x = self.x + self.dx
